354b26d9f956b1ef48033491c03362f65ae0e577
kellerliu666 <keller.liu@hotmail.com>
2016-09-19 01:29:16 -0400

    Update 1-Python-Primer-Essential.py

--- 1-Python-Primer-Essential.py
+++ 1-Python-Primer-Essential.py
@@ -4,7 +4,8 @@ sorted(data)
 # When classes define one or more methods:
 data.sort()
 
-# But be aware, some methods, like <sorted(data)>, only made a sorted copy from data and returned the sorted copy. However, some methods, like <data.sort()>, changed data itself.
+# But be aware, some methods, like <sorted(data)>, only made a sorted copy from data and returned the sorted copy. 
+# However, some methods, like <data.sort()>, changed data itself.
 >>> data = [2, 6, 4, 3, 5, 8, 7, 0, 1]
 >>> print sorted(data)
 >>> print data
