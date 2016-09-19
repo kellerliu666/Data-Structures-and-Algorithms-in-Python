393f9cdf186ff6fcd6b83247c09585049b323ec7
kellerliu666 <keller.liu@hotmail.com>
2016-09-19 01:20:03 -0400

    Update 1-Python-Primer-Essential.py

--- 1-Python-Primer-Essential.py
+++ 1-Python-Primer-Essential.py
@@ -1,2 +1,9 @@
 # Invoke function in traditional way, like:
 sorted(data)
+# When classes define one or more methods:
+data.sort()
+# But be aware, sorted(data) only made a copy from data and returned the sorted copy.
+data = [2,6,4,3,5,8,7,0,1]
+print sorted(data)
+print data
+# data.sort() changed data itself.
