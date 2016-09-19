9358c86646d459679d438634290a4932fc84f298
kellerliu666 <keller.liu@hotmail.com>
2016-09-19 03:56:28 -0400

    Update 1-Python-Primer-Essential.py

--- 1-Python-Primer-Essential.py
+++ 1-Python-Primer-Essential.py
@@ -4,13 +4,13 @@ Chapter 1 Python Primer
 # Be aware, some methods, like <sorted(list)>, only made a sorted copy from data and returned the sorted copy. 
 # However, some methods, like <list.sort()>, changed data itself.</p>
 >>> a_lst = [2, 6, 4, 3, 5, 8, 7, 0, 1]
->>> print sorted(a_lst)
-[0, 1, 2, 3, 4, 5, 6, 7, 8]
+>>> print sorted(a_lst, reverse = True)
+[8, 7, 6, 5, 4, 3, 2, 1, 0]
 >>> print a_lst
 [2, 6, 4, 3, 5, 8, 7, 0, 1]
->>> a_lst.sort()
+>>> a_lst.sort(reverse = True)
 >>> print a_lst
-[0, 1, 2, 3, 4, 5, 6, 7, 8]
+[8, 7, 6, 5, 4, 3, 2, 1, 0]
 
 #--------------------------------------------------------------------------------------------------------------
 # Some methods that don't change original data.
