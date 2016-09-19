41fb881a0a7f2f522b5fe6c7546d152148e6505c
kellerliu666 <keller.liu@hotmail.com>
2016-09-19 01:54:35 -0400

    Update and rename 1-Python-Primer-Essential.htm to 1-Python-Primer-Essential.py

--- 1-Python-Primer-Essential.py
+++ 1-Python-Primer-Essential.py
@@ -1,8 +1,7 @@
-<!DOCTYPE html>
-<html>
-<body>
-<h1>Chapter One -- Python Primer</h1>
-<p># Be aware, some methods, like <sorted(list)>, only made a sorted copy from data and returned the sorted copy. 
+
+Chapter One -- Python Primer
+
+# Be aware, some methods, like <sorted(list)>, only made a sorted copy from data and returned the sorted copy. 
 # However, some methods, like <list.sort()>, changed data itself.</p>
 >>> a_lst = [2, 6, 4, 3, 5, 8, 7, 0, 1]
 >>> print sorted(a_lst)
@@ -25,5 +24,4 @@ aaaabbbbbccc
 >>> print a_str
 aaaabbbbbaaa
 aaaabbbbbccc
-</body>
-</html>
+
