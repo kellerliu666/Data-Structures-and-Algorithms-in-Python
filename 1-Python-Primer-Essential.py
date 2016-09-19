ec47a36d9cb3d4f21f5357344c04567936161ae3
kellerliu666 <keller.liu@hotmail.com>
2016-09-19 01:45:14 -0400

    Update 1-Python-Primer-Essential.py

--- 1-Python-Primer-Essential.py
+++ 1-Python-Primer-Essential.py
@@ -1,16 +1,23 @@
-# Invoke function in traditional way, like:
-sorted(data)
-
-# When classes define one or more methods:
-data.sort()
-
-# But be aware, some methods, like <sorted(data)>, only made a sorted copy from data and returned the sorted copy. 
-# However, some methods, like <data.sort()>, changed data itself.
->>> data = [2, 6, 4, 3, 5, 8, 7, 0, 1]
->>> print sorted(data)
->>> print data
+# Be aware, some methods, like <sorted(list)>, only made a sorted copy from data and returned the sorted copy. 
+# However, some methods, like <list.sort()>, changed data itself.
+>>> a_lst = [2, 6, 4, 3, 5, 8, 7, 0, 1]
+>>> print sorted(a_lst)
+>>> print a_lst
 [0, 1, 2, 3, 4, 5, 6, 7, 8]
 [2, 6, 4, 3, 5, 8, 7, 0, 1]
->>> data.sort()
->>> print data
+>>> a_lst.sort()
+>>> print a_lst
 [0, 1, 2, 3, 4, 5, 6, 7, 8]
+
+# Some methods that don't change original data.
+>>> import string
+>>> a_str = 'aaaabbbbbccc'
+>>> table = string.maketrans('c', 'a')
+>>> print a_str.translate(table)
+>>> print a_str
+aaaabbbbbaaa
+aaaabbbbbccc
+>>> print a_str.replace('c', 'a')
+>>> print a_str
+aaaabbbbbaaa
+aaaabbbbbccc
