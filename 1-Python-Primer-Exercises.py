b207120ede5d7659edb73caa277a2e22866299b3
kellerliu666 <keller.liu@hotmail.com>
2016-09-19 04:44:07 -0400

    Update 1-Python-Primer-Exercises.py

--- 1-Python-Primer-Exercises.py
+++ 1-Python-Primer-Exercises.py
@@ -19,3 +19,50 @@ import string
 for p in string.punctuation:
     s = s.replace(p, " ")
 print s
+
+#--------------------------------------------------------------------------------------------------------------
+# Write a Python program that can “make change.” Try to design your program so that it returns as few bills and 
+# coins as possible.
+change = float(input(Enter amount of change: ))
+lst = [100.0, 50.0, 20.0, 10.0, 5.0, 1.0, 0.25, 0.1, 0.05, 0.01]
+for money in lst:
+    count = 0
+    if money > change: continue
+    while True:
+        if money > change:
+            break
+        if money <= change: 
+            count += 1
+            change -= money
+    print '$', money, 'x', count    # Something wrong with it, try enter 0.03 or 21.22. Feel free correct it
+
+#--------------------------------------------------------------------------------------------------------------
+# Design a program that can test Birthday Paradox by a series of experiments on randomly generated birthdays.
+import random
+
+def check(birthlst):
+    counts = dict()
+    for item in birthlst:
+        counts[item] = counts.get(item, 0) + 1
+        if counts[item] > 1:
+            return True
+    return False
+
+loop = 10000                                        # I did 10000 experiments here
+total = float(loop)
+yes = 0
+while True:
+    if loop == 0: break
+    birthlst = list()
+    n = 23
+    while True:
+        if n == 0: break
+        date = random.randint(1, 365)
+        birthlst.append(date)
+        n -= 1
+    if check(birthlst): yes += 1
+    loop -= 1
+prob = yes / total
+print prob
+
+#--------------------------------------------------------------------------------------------------------------
