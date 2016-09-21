009f8d9aabc9a142bd2bc23eb7ade95bcbfbc4c4
kellerliu666 <keller.liu@hotmail.com>
2016-09-21 01:07:33 -0400

    Update 2-Object-Oriented-Programming-Essential.py

--- 2-Object-Oriented-Programming-Essential.py
+++ 2-Object-Oriented-Programming-Essential.py
@@ -1,4 +1,12 @@
 #--------------------------------------------------------------------------------------------------------------
 Chapter 2 Object-Oriented Programming
 #--------------------------------------------------------------------------------------------------------------
-#
+# Form some good coding style in Python.
+# Use 4 apaces to indent instead of using Tab.
+# Capitalized single noun would be the best choice for Class name.
+class Employee or CreditCard:
+  
+# Usually choose verb in lowercase as Function name.
+def make_payment():
+  
+# 
