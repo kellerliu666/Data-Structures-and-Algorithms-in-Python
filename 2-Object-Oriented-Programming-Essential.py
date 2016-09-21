c4a75e79de716db44108873e8edd5c7d81a6001e
kellerliu666 <keller.liu@hotmail.com>
2016-09-21 19:14:15 -0400

    Update 2-Object-Oriented-Programming-Essential.py

--- 2-Object-Oriented-Programming-Essential.py
+++ 2-Object-Oriented-Programming-Essential.py
@@ -11,32 +11,41 @@ class Employee or CreditCard:
 def make_payment():
   
 #--------------------------------------------------------------------------------------------------------------  
-# Here is a simple and basic example of creating class, which is CreditCard:
-class CreditCard:    
-    def __init__(self, name, bank, account, limit):
-        self._name = name        # Differ to name, _name indicate that it's nonpublic 
-        self._bank = bank
-        self._account = account
-        self._limit = limit
-        self._balance = 0        
-    def get_customer(self):
-        return self._name        
-    def get_bank(self):
-        return self._bank        
-    def get_account(self):
-        return self._account       
+# Here is a simple and basic example of creating class, which is BankAccount:
+class BankAccount:
+    """ Class definition modeling the behavior of a simple bank account """
+    count = 0
+    def __init__(self, initial_balance):
+        self.initial_balance = initial_balance
+        """Creates an account with the given balance."""
+
+    def deposit(self, amount):
+        self.initial_balance += amount
+        """Deposits the amount into the account."""
+
+    def withdraw(self, amount):
+        """
+        Withdraws the amount from the account. Each withdrawal resulting 
+        in a negative balance also deducts a penalty fee of 5 dollars
+        from the balance.
+        """
+        self.initial_balance -= amount
+        if self.initial_balance < 0:
+            self.initial_balance -= 5
+            self.count += 1
+            
     def get_balance(self):
-        return self._balance       
-    def get_limit(self):
-        return self._limit    
-    def make_payment(self, amount):
-        self._balance -= amount        
-    def charge(self, price):
-        if price + self._balance > self._limit:
-            return False
-        else: 
-            self._balance += price
-            return True
+        """Returns the current balance in the account."""
+        return self.initial_balance
+        
+    def get_fees(self):
+        """Returns the total fees ever deducted from the account."""
+        return self.count*5
+        
+my_account = BankAccount(10)
+my_account.withdraw(15)
+my_account.deposit(20)
+print my_account.get_balance(), my_account.get_fees()
 
 #--------------------------------------------------------------------------------------------------------------
 #
