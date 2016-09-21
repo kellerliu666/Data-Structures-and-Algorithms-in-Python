31c9168da2ca59b30f77b37b67727731b9c2fc99
kellerliu666 <keller.liu@hotmail.com>
2016-09-21 03:28:52 -0400

    Update 2-Object-Oriented-Programming-Essential.py

--- 2-Object-Oriented-Programming-Essential.py
+++ 2-Object-Oriented-Programming-Essential.py
@@ -12,30 +12,30 @@ def make_payment():
   
 #--------------------------------------------------------------------------------------------------------------  
 # Here is a simple and basic example of creating class, which is CreditCard:
-class CreditCard:
+class CreditCard:    
     def __init__(self, name, bank, account, limit):
-        self.name = name
-        self.name = bank
-        self.account = account
-        self.limit = limit
-        self.balance = 0
+        self._name = name
+        self._bank = bank
+        self._account = account
+        self._limit = limit
+        self._balance = 0        
     def get_customer(self):
-        return self.name
+        return self._name        
     def get_bank(self):
-        return self.bank
+        return self._bank        
     def get_account(self):
-        return self.account
+        return self._account       
     def get_balance(self):
-        return self.balance
+        return self._balance       
     def get_limit(self):
-        return self.limit
+        return self._limit    
     def make_payment(self, amount):
-        self.balance -= amount
+        self._balance -= amount        
     def charge(self, price):
-        if price + self.balance > self.limit:
+        if price + self._balance > self._limit:
             return False
         else: 
-            self.balance += price
+            self._balance += price
             return True
 
 #--------------------------------------------------------------------------------------------------------------
