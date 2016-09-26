37e2a2c0e3d3383496a04f83da54df340f6b09b1
kellerliu666 <keller.liu@hotmail.com>
2016-09-25 21:46:06 -0400

    Update 2-Object-Oriented-Programming-Essential.py

--- 2-Object-Oriented-Programming-Essential.py
+++ 2-Object-Oriented-Programming-Essential.py
@@ -55,4 +55,50 @@ if __name__ == '__main__':
 # imported for use in a larger software project.
 
 #--------------------------------------------------------------------------------------------------------------
+# When define a class, there are many Special Methods to indicate Common Syntax, like:
+__add__(self, other): self + other
+__mul__(self, other): self * other
+__getitem__(self, k): self.a[k]
+__setitem__(self, k, v): self.a[k] = v
+__len__(self): len(self)
+# etc...etc...etc...
+
+#--------------------------------------------------------------------------------------------------------------
+# Here is an example of some Special Methods:
+class Gen:
+    '''Create a class named Gen'''
+    def __init__(self, d):
+        '''Set digits of Gen class'''
+        self.coords = [0] * d
+    def __len__(self):
+        '''Return length of digits of Gen'''
+        return len(self.coords)
+    def __setitem__(self, i, val):
+        '''Set given value to certain position'''
+        self.coords[i] = val
+    def __getitem__(self, i):
+        '''Return given value at certain position'''
+        return self.coords[i]
+    def __mul__(self, other):
+        '''Return multiple of two Gen class'''
+        result = Gen(len(self.coords))
+        for j in range(len(self.coords)):
+            result[j] = self.coords[j] * other.coords[j]
+        return result
+    def __str__(self):
+        '''Present Gen class by certain string'''
+        return '<' + str(self.coords)[1:-1] + '>'
+
+if __name__ == '__main__':
+    '''Testing'''
+    G1 = Gen(2)  # Create <0,0>
+    G2 = Gen(2)  # Create <0,0>
+    G1[0] = 2    
+    G1[1] = 2    # <2,2>
+    G2[0] = 3    
+    G2[1] = 4    # <3,4>
+    Gr = G1 * G2 # <6,8>
+    print Gr     # print out <6,8>
+    
+#--------------------------------------------------------------------------------------------------------------
 #
